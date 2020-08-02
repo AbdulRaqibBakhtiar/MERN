e3f11372a6bec26db1e72cc10d141c46707f3f09
Abdul Raqib <abdulraqib.bakhtiar@gmail.com>
2020-08-02 10:24:19 +0530

    or your changes. Lines starting

---
+++ config/keys.js
@@ -0,0 +1,3 @@
+module.exports = {
+    mongoURI: 'mongodb://abdulraqibDB:Pulsar1234@cluster0-shard-00-00-hllwg.mongodb.net:27017,cluster0-shard-00-01-hllwg.mongodb.net:27017,cluster0-shard-00-02-hllwg.mongodb.net:27017/<dbname>?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true&w=majority'
+}
---
+++ server.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const path = require('path');
+
+const items = require('./routes/api/items'); 
+
+const app = express();
+
+app.use(bodyParser.json());
+
+const db = require('./config/keys').mongoURI;
+
+
+mongoose.connect(db)
+.then(()=> console.log('MongoDB Connected...'))
+.catch(err => console.log(err));
+
+app.use('/api/items',items);
+
+if(process.env.NODE_ENV === 'production'){
+    app.use(express.static('client/build'));
+
+    app.get('*', (req, res) => {
+        res.sendFile(path.resolve(__dirname, 'client', 'build','index.html'));
+    });
+}
+
+const Port = process.env.PORT || 5000;
+
+app.listen(Port, () => console.log(`Server started on port ${Port}`));
